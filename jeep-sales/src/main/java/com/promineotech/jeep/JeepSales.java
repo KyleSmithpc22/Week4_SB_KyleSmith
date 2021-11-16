4e6b6d023179b06f3b3754943a1706c54478d063
Kyle J Smith <80004262+KyleSmithpc22@users.noreply.github.com>
2021-11-16 01:19:38 -0500

    Done?

---
+++ jeep-sales/src/main/java/com/promineotech/jeep/JeepSales.java
@@ -0,0 +1,15 @@
+package com.promineotech.jeep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeepSales {
+
+  // http://localhost:8080/swagger-ui.html
+
+  public static void main(String[] args) {
+    SpringApplication.run(JeepSales.class, args);
+  }
+}
+
